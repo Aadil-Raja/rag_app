ee385dbe70c4da5eeafa709ab27e9a8074855236
Aadil Raja <138008487+Aadil-Raja@users.noreply.github.com>
2025-05-09 16:17:20 +0500

    context lenght

--- app/llm.py
+++ app/llm.py
@@ -4,8 +4,8 @@ from ctransformers import AutoModelForCausalLM
 llm = AutoModelForCausalLM.from_pretrained(
     "models/mistral-7b-instruct-v0.1.Q2_K.gguf",
     model_type="mistral",
-    max_new_tokens=2048,
-    context_length=4096,  # Explicitly set a higher context length
+    max_new_tokens=512,
+    context_length=1024,  # Explicitly set a higher context length
     threads=6
 )
 
